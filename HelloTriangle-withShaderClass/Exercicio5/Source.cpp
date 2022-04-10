0460adf0bd847957c36e695644878fa7adf13171
Eduardo Arruda Remião <66395784+duduremiao@users.noreply.github.com>
2022-04-09 22:23:01 -0300

    Ex1 lista 2

--- HelloTriangle-withShaderClass/Exercicio5/Source.cpp
+++ HelloTriangle-withShaderClass/Exercicio5/Source.cpp
@@ -18,7 +18,9 @@ using namespace std;
 
 // GLFW
 #include <GLFW/glfw3.h>
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "Shader.h"
 
 
